7a54527b95fdbafb7ec68072befa168b0f895007
qdb048sta <56998170+qdb048sta@users.noreply.github.com>
2021-03-31 16:44:27 +0800

    correct answer

--- 65. Valid Number.py
+++ 65. Valid Number.py
@@ -1,4 +1,49 @@
 class Solution:
     def isNumber(self, s: str) -> bool:
-        return True
-x=10
+        if s.count("e")==1 :
+            l=s.split("e")
+            if l[1].count("+")==1 or l[1].count("-")==1 or ("+" not in l[1] and "-" not in l[1]):
+                try:
+                    int(l[1])
+                    if l[0].count("+")==1 or l[0].count("-")==1 or ("+" not in l[0] and "-" not in l[0]):
+                            try:
+                                float(l[0])
+                                return True
+                            except:
+                                return False
+                    else:
+                        return False
+                    
+                except:
+                    return False
+            else:
+                return False
+        elif s.count("E")==1:
+            l=s.split("E")
+            if l[1].count("+")==1 or l[1].count("-")==1 or ("+" not in l[1] and "-" not in l[1]):
+                try:
+                    int(l[1])
+                    if l[0].count("+")==1 or l[0].count("-")==1 or ("+" not in l[0] and "-" not in l[0]):
+                            try:
+                                float(l[0])
+                                return True
+                            except:
+                                return False
+                    else:
+                        return False
+                    
+                    
+                except:
+                    return False
+        elif "inf" in s.lower():
+            return False
+        else:
+            if s.count("+")==1 or s.count("-")==1 or ("+" not in s and "-" not in s):
+                    try:
+                        float(s)
+                        return True
+                    except:
+                        return False
+            else:
+                return False
+        
