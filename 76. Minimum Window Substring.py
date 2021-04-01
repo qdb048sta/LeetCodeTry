7707a7e767ab95952a1fe55c6b1d4486842b4030
qdb048sta <56998170+qdb048sta@users.noreply.github.com>
2021-04-01 13:22:26 +0800

    Update 76. Minimum Window Substring.py

--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -3,13 +3,8 @@ class Solution:
         record=[]
         for i in t:
             indexing=s.find(i)
-            record.append(indexing)
-        if -1 in record:
-            return ""
-        else:
-            record=sorted(record)
-            maximum=record[-1]
-            minimum=record[0]
-            substring=s[minimum:maximum]
-            return substring
+            if indexing==-1:
+                break
+                return ""
+            
         
