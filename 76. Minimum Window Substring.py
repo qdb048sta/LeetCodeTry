34b3df520d8a27adb00ab0bb59c7610279d56beb
qdb048sta <56998170+qdb048sta@users.noreply.github.com>
2021-05-13 11:16:11 +0800

    Update 76. Minimum Window Substring.py

--- 76. Minimum Window Substring.py
+++ 76. Minimum Window Substring.py
@@ -1,10 +1,20 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        record=[]
+        record={}
         for i in t:
             indexing=s.find(i)
             if indexing==-1:
                 break
                 return ""
+            if i not in record:
+                record[i]=[indexing]
+            else:
+                record[i]=record[i].append(indexing)
+        numdist=[]
+        for k in record.keys():
+            numdist=numdist.append(record[k])
+        for i in numdist:
+            sublength=len(i)
+            for            
             
         
