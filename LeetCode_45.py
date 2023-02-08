0510a7d829d78fb7762c69637579ed8a3d736040
qdb048sta <2169051w@gmail.com>
2023-02-08 17:44:51 +0800

    add some thought on LeetCode_45

---
+++ LeetCode_45.py
@@ -0,0 +1,5 @@
+#LeetCode45
+#example_1=[2,3,1,1,4]2
+#example_2=[2,3,0,1,4]2
+'''first thought: Transformed to the dijika Algorithm
+first for first point it extend several edges based on the number , and make a connected graph''' 
